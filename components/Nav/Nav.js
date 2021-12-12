b64d89579795dc7c9831ef0faf114e6f683c47be
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-12-12 20:39:23 +0545

    Added a nav bar

---
+++ components/Nav/Nav.js
@@ -0,0 +1,9 @@
+export default function Nav() {
+    return (
+        <nav className='navbar navbar-light bg-light'>
+            <div className='container-fluid'>
+                <span className='navbar-brand mb-0 h1'>Navbar</span>
+            </div>
+        </nav>
+    )
+}
