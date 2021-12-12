0f18d3212abb6bf5a075b7d018ed4f80b552842d
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-12-12 20:55:23 +0545

    Created a new cats page

---
+++ pages/cats/index.js
@@ -0,0 +1,11 @@
+import Nav from "../../components/Nav/Nav"
+
+export default function cats() {
+    return (
+        <div>
+            <title>Cats</title>
+
+            <Nav />
+        </div>
+    )
+}
