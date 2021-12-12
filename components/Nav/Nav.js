d30852c2846d3131fc50e4618bf8ff3ca7d42e2f
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-12-12 21:33:22 +0545

    Updated nav bar

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,9 +1,13 @@
+import Link from "next/link"
+import React from "react"
+import styles from "./Nav.module.css"
+
 export default function Nav() {
     return (
         <nav className='navbar navbar-light bg-light'>
-            <div className='container-fluid'>
-                <span className='navbar-brand mb-0 h1'>Navbar</span>
-            </div>
+            <Link className={styles["nav-link"]} href='/'>
+                Navbar
+            </Link>
         </nav>
     )
 }
